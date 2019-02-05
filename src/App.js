f03f2c271959ef677b7c56baadcc835eb1f9407f
hemali pateliya <hemali.pateliya@gmail.com>
2019-02-04 17:01:24 -0800

    first commit -

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+
+  state={
+
+    name:'',
+    age:''
+  }
+
+
+  showData=()=>
+  {
+
+    this.setState({
+        name:this.name.value,
+        age:this.age.value
+
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+        <span> Name:</span><input type="text" ref={(input)=>{this.name=input}}/>
+        <span> Age:</span><input type="text" ref={(input)=>{this.age=input}}/>
+        <input type="submit" onClick={this.showData}/>
+        </form>
+        <h4>Name :{this.state.name} Age:{this.state.age}</h4>
+
       </div>
     );
   }
